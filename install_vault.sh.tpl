2bfaddd992a9298873c3a33d1270c75a7db26c01
Craig Snow <craigs@ignw.io>
2023-03-08 16:21:35 -0800

    changed to new version and tested in project

--- install_vault.sh.tpl
+++ install_vault.sh.tpl
@@ -6,16 +6,16 @@ export local_ipv4="$(curl -s http://metadata.google.internal/computeMetadata/v1/
 
 # install package
 
-#curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-#apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-#apt-get update
-#apt-get install -y vault-enterprise=${vault_version}+ent jq
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update
+apt-get install -y vault-enterprise=${vault_version}+ent-1 jq
 
 
 #RHEL installation information
-yum install -y yum-utils
-yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-yum -y install vault-enterprise-${vault_version}+ent-1 jq
+#yum install -y yum-utils
+#yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+#yum -y install vault-enterprise-${vault_version}+ent-1 jq
 
 systemctl stop firewalld
 systemctl disable firewalld
--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ data "google_compute_subnetwork" "vault-network-primary" {
 module "vault-ent-primary" {
   source               = "hashicorp/vault-ent-starter/gcp"
   #source               = "./modules/vault-starter"
-  version              = "0.1.2"
+  #version              = "0.1.2"
 
   # The shared DNS SAN of the TLS certs being used
   leader_tls_servername  = "primary-vault.server.com"
@@ -37,16 +37,16 @@ module "vault-ent-primary" {
   #reserve subnet range
   reserve_subnet_range   = "10.1.0.0/16"
   # Name of the SSL Certificate to be used for Vault LB
-  ssl_certificate_name   = "vault-primary-20220513213251761400000001"
+  ssl_certificate_name   = "vault-primary-20230308223017255900000001"
   # Secret id/name given to the google secret manager secret
   tls_secret_id          = "terraform_example_module_vault_tls_secret"
   # Path to Vault Enterprise license file
   vault_license_filepath = "/Users/csnow/git/vault-sip/vault-gke/tls/vault-enterprise.hclic"
   # Vault Version
-  #vault_version          = "1.10.2-1"
-  vault_version          = "1.10.2"
+  vault_version          = "1.13.0"
+  #vault_version          = "1.13.0"
   #disk_source_image    = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts"
-  vm_disk_source_image    = "rhel-8-v20220406"
+  #vm_disk_source_image    = "rhel-8-v20220406"
   # Node Count 	
   node_count		= 3
   # User Data script
